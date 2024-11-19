2b8020f7df76c5f541f75e85f116282eb85621a2
LizzyJoss <joselinespin01@gmail.com>
2024-11-19 11:56:49 -0500

    add_Bar_ITem

--- lib/menu.dart
+++ lib/menu.dart
@@ -5,12 +5,21 @@ class Menu extends StatelessWidget { //crear la clase login
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Bienvenido',
-          style: TextStyle(
-            color: Colors.white
-          ),
+        title: Row(
+          children: [
+            const Text('CES SHOW',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            Icon(
+              Icons.book_online_outlined,
+              color: Colors.white,
+              size: 30,
+            )
+          ],
         ),
-        backgroundColor: Colors.deepPurple
+        backgroundColor: Color.fromARGB(255, 105, 31, 145)
       ),
       body: Center(
         child: Text('Bienvenido',
@@ -19,6 +28,37 @@ class Menu extends StatelessWidget { //crear la clase login
           ),
         ),
       ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Color.fromARGB(255, 105, 31, 145),
+        selectedItemColor: Colors.white,
+        
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'MENU',
+            
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.event),
+            label: 'TEATRO',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.people_rounded),
+            label: 'CONCIERTO',
+          ),
+        ],
+        onTap: (index) {
+          if (index == 0) {
+            Navigator.pushNamed(context,'/menu'); 
+          } else if (index == 1) {
+            Navigator.pushNamed(context, '/teatro'); 
+          } else if (index == 2) {
+            Navigator.pushNamed(context, '/concierto'); 
+          }
+        },
+      ),
+           
     );
   }
   
