d7b1edd80db4a4f3da2f6808ed3ab533951f95d0
LizzyJoss <joselinespin01@gmail.com>
2024-11-19 21:51:51 -0500

    fix_menu

--- lib/views/inicio/menu.dart
+++ lib/views/inicio/menu.dart
@@ -25,21 +25,22 @@ class Menu extends StatelessWidget { //crear la clase login
       body: Center(
         child: Column(
           children: [
-            CarouselSlider(
-              items: [
-                Image.network('https://i.imgur.com/yTOZEAK.jpeg'),
-                Image.network('https://i.imgur.com/TFV0wTK.png'),
-                Image.network('https://i.imgur.com/khG9Axe.png')
-
-              ], 
-              options: CarouselOptions(
-                height: 130,  //ancho
-                autoPlay: true, //automatico
-                enlargeCenterPage: true //centro
-
-              )
-            ),
-            SizedBox(height: 5),
+            
+              CarouselSlider(
+                items: [
+                  Image.network('https://i.imgur.com/yTOZEAK.jpeg',width: 1500),
+                  Image.network('https://i.imgur.com/TFV0wTK.png',width: 1500),
+                  Image.network('https://i.imgur.com/khG9Axe.png',width: 1500,)
+                ], 
+                options: CarouselOptions(
+                  height: 190,
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  viewportFraction: 1.5,
+                )
+              ),
+            
+            SizedBox(height: 3),
             Container(
               color: Colors.black,
               child: Column(
@@ -57,7 +58,7 @@ class Menu extends StatelessWidget { //crear la clase login
                       Expanded(
                         child: Column(
                           children: [
-                            Image.network('https://i.imgur.com/i4tTM7C.png',height: 180,),
+                            Image.network('https://i.imgur.com/i4tTM7C.png',height: 200,),
                             SizedBox(height: 10),
                             const Text('Kudai Revive Tour',
                               style: TextStyle(
@@ -108,57 +109,7 @@ class Menu extends StatelessWidget { //crear la clase login
                       Expanded(
                         child: Column(
                           children: [
-                            Image.network('https://i.imgur.com/kBHrafp.png',height: 180,),
-                            SizedBox(height: 10),
-                            const Text('Los Kiños Del Año',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold
-                              ),
-                            ),
-                            
-                            Row(
-                              children: [
-                                Icon(
-                                  Icons.location_on,
-                                  color: Colors.white70,
-                                ),
-                                const Text('Coliseo General Rumiñahui',
-                                  style: TextStyle(
-                                    color: Colors.white70,
-                                    fontSize: 10
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Icon(
-                                  Icons.calendar_today_sharp,
-                                  color: Colors.white70,
-                                ),
-                                const Text('30 de Noviembre 2024',
-                                  style: TextStyle(
-                                    color: Colors.white70,
-                                    fontSize: 10
-                                  ),
-                                ),
-                              ],
-                            ),
-                            ElevatedButton(
-                              onPressed: (){},
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: Color.fromARGB(255, 105, 31, 145)
-                              ), 
-                              child: const Text('VER EVENTO',style:TextStyle(color: Colors.white)),
-                            )
-                          ],
-                        ),
-                      ),
-                      Expanded(
-                        child: Column(
-                          children: [
-                            Image.network('https://i.imgur.com/sc0Wg8F.png',height: 180,),
+                            Image.network('https://i.imgur.com/sc0Wg8F.png',height: 200,),
                             SizedBox(height: 10),
                             const Text('Ha Ash',
                               style: TextStyle(
